6aec2cb440f191294440fcd5ea8867e5a5a0ee4a
karmaicom <karmaicom@gmail.com>
2025-08-24 16:27:04 -0300

    Commit initial

---
+++ src/main/java/com/devsuperior/dslist/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dslist.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("")
+    public String hello() {
+        return "Olá, mundo!";
+    }
+	
+}
